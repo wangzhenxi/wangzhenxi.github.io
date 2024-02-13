f6a86ec484c2e24de4ccefeb16f6910921fee09b
Josh <im@joshwong.cn>
2024-02-13 11:04:49 +0800

    update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,10 +1,9 @@
+const fs = require('fs')
+const path = require('path')
+const markdownIt = require('markdown-it')
+const meta = require('markdown-it-meta')
 
-const fs = require('fs');
-const path = require('path');
-const markdownIt = require("markdown-it");
-const meta = require("markdown-it-meta");
-
-const rootPath = path.join(__dirname, '../');
+const rootPath = path.join(__dirname, '../')
 
 function genderRoute({ title, path, children, filepath }) {
     if (children && children.length) {
@@ -14,11 +13,11 @@ function genderRoute({ title, path, children, filepath }) {
         }
     }
     if (filepath) {
-        const content = fs.readFileSync(filepath, 'utf-8');
-        const md = new markdownIt();
-        md.use(meta);
-        md.render(content);
-        if (md.meta.hide) return;
+        const content = fs.readFileSync(filepath, 'utf-8')
+        const md = new markdownIt()
+        md.use(meta)
+        md.render(content)
+        if (md.meta.hide) return
     }
     return {
         title,
@@ -27,17 +26,15 @@ function genderRoute({ title, path, children, filepath }) {
 }
 
 const roll = (base, parentFilename) => {
-    const files = fs
-        .readdirSync(base)
-        .filter(item => !/^\./.test(item));
-    const dirs = files.map(filename => {
-        const filepath = path.join(base, filename);
-        const fileStat = fs.statSync(filepath);
+    const files = fs.readdirSync(base).filter((item) => !/^\./.test(item))
+    const dirs = files.map((filename) => {
+        const filepath = path.join(base, filename)
+        const fileStat = fs.statSync(filepath)
         if (fileStat.isDirectory()) {
-            let children = roll(filepath, filename).filter(item => !!item);
-            if (!children.length) return;
-            if (children.some(item => item === 'README.md')) {
-                const basepath = `${path.join(base, filename)}`;
+            let children = roll(filepath, filename).filter((item) => !!item)
+            if (!children.length) return
+            if (children.some((item) => item === 'README.md')) {
+                const basepath = `${path.join(base, filename)}`
                 return genderRoute({
                     title: filename,
                     filepath: `${basepath}/README.md`,
@@ -49,7 +46,7 @@ const roll = (base, parentFilename) => {
                 children
             })
         } else if (filename !== 'README.md') {
-            const basepath = `${path.join(base, filename)}`;
+            const basepath = `${path.join(base, filename)}`
             return genderRoute({
                 title: parentFilename,
                 filepath: basepath,
@@ -57,49 +54,95 @@ const roll = (base, parentFilename) => {
             })
         } else {
             if (parentFilename) {
-                return 'README.md';
+                return 'README.md'
             } else {
                 return genderRoute({
                     title: '关于我',
                     path: '/'
-                });
+                })
             }
         }
     })
-    return dirs;
+    return dirs
 }
 
-const sidebar = roll(rootPath).filter(item => !!item);
+const sidebar = roll(rootPath).filter((item) => !!item)
 
 module.exports = {
     // 站点配置
     lang: 'zh-CN',
     title: 'Joshwong | 王震西',
     description: '记录点滴',
-    head: [
-    ],
+    head: [],
     plugins: [
         // 百度统计
-        ['vuepress-plugin-baidu-tongji-analytics', {
-            key: '05011c0833edb057b092ef78bbaf7ff5',
-        }],
+        [
+            'vuepress-plugin-baidu-tongji-analytics',
+            {
+                key: '05011c0833edb057b092ef78bbaf7ff5'
+            }
+        ],
 
         // 评论
-        ['@vssue/vuepress-plugin-vssue', {
-            // 设置 `platform` 而不是 `api`
-            platform: 'github',
+        [
+            '@vssue/vuepress-plugin-vssue',
+            {
+                // 设置 `platform` 而不是 `api`
+                platform: 'github',
 
-            // 其他的 Vssue 配置
-            owner: 'wangzhenxi',
-            repo: 'wangzhenxi.github.io',
-            clientId: '0deb5b15f58c92d3fcf4',
-            clientSecret: '97ffc1e39e11dbccb1cf481b58b435e2dc3b87cc',
-        }],
+                // 其他的 Vssue 配置
+                owner: 'wangzhenxi',
+                repo: 'wangzhenxi.github.io',
+                clientId: '0deb5b15f58c92d3fcf4',
+                clientSecret: '97ffc1e39e11dbccb1cf481b58b435e2dc3b87cc'
+            }
+        ],
 
         // 站点地图
-        ['sitemap', {
-            hostname: 'https://www.wangzhenxi.com'
-        }]
+        [
+            'sitemap',
+            {
+                hostname: 'https://www.wangzhenxi.com'
+            }
+        ],
+
+        // 爬虫
+        [
+            'robots',
+            {
+                /**
+                 * @host
+                 * Mandatory, You have to provide the host URL
+                 */
+                host: 'https://www.wangzhenxi.com',
+                /**
+                 * @disallowAll
+                 * Optional: if it's true, all others options are ignored and exclude all robots from the entire server
+                 */
+                disallowAll: false,
+                /**
+                 * @allowAll
+                 * Optional: if it's true and @disallowAll is false, all others options are ignored and allow all robots complete access
+                 */
+                allowAll: true,
+                /**
+                 * @sitemap
+                 * Optional, by default: sitemap.xml
+                 */
+                sitemap: '/sitemap.xml'
+                /**
+                 * @policies
+                 * Optional, by default: null
+                 */
+                // policies: [
+                //     {
+                //         userAgent: '*',
+                //         disallow: [],
+                //         allow: ['/']
+                //     }
+                // ]
+            }
+        ]
     ],
 
     // 主题和它的配置
@@ -107,10 +150,8 @@ module.exports = {
     themeConfig: {
         smoothScroll: true,
         search: false,
-        nav: [
-            { text: 'Github', link: 'https://github.com/wangzhenxi' },
-        ],
+        nav: [{ text: 'Github', link: 'https://github.com/wangzhenxi' }],
         sidebar,
-        lastUpdated: 'Last Updated', // string | boolean
-    },
+        lastUpdated: 'Last Updated' // string | boolean
+    }
 }
